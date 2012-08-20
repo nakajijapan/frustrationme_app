5e828b6414aaf7244bc17b5468ac6f40c3bf90e1
nakajijapan <nakajima@paperboy.co.jp>
2012-08-20 14:38:43 +0900

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frustration::Application.config.secret_token = 'ed80f9e3a15696e3bd89c2d5de61dccf4e791721f2ce7b8ed34d4e91c5cab2d8b2aff236411544d78252649556b88b8bdd124533e26c0dde79b144d1b3845b03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Frustration::Application.config.session_store :cookie_store, key: '_frustration_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Frustration::Application.config.session_store :active_record_store
