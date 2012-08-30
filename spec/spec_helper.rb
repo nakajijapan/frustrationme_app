8e97b73a2cd7dc450d852e7005d081234e3e2db2
nakajijapan <nakajima@paperboy.co.jp>
2012-08-30 22:03:27 +0900

    test code for model with rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,29 @@
+ENV["RAILS_ENV"] ||= 'test'
+
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.include FactoryGirl::Syntax::Methods
+
+end
