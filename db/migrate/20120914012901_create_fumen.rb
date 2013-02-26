43bbff5ff2720bd421ac074f0f74e582f2d4de4f
nakajijapan <nakajima@paperboy.co.jp>
2013-02-26 15:14:03 +0900

    no needs priority

--- db/migrate/20120914012901_create_fumen.rb
+++ db/migrate/20120914012901_create_fumen.rb
@@ -1,12 +1,11 @@
 class CreateFumen < ActiveRecord::Migration
   def change
-    create_table :fumen do |t|
+    create_table :fumans do |t|
       t.integer :user_id, :null => false
       t.datetime :date
       t.string :title
       t.integer :price, :default => 0
       t.text :content
-      t.integer :priority, :default => 2
       t.integer :status, :default => 1
       t.text :tag_ids
       t.integer :category_id
