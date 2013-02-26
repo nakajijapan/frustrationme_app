698b9c414a1d3974f6a2245af6f6dc661fc2541c
nakajijapan <nakajima@paperboy.co.jp>
2013-02-26 15:01:18 +0900

    config

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+if ENV['RAILS_ENV'] == 'production'
+  require File.expand_path('../../../shared/secret', __FILE__)
+else
+  require File.expand_path('../secret', __FILE__)
+end
+
 # Initialize the rails application
 Frustration::Application.initialize!
 
