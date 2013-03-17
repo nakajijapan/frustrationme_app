290760b4f1b488acefc6efdc8f7b5f7d791765da
nakajijapan <nakajima@paperboy.co.jp>
2013-03-18 03:18:19 +0900

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -8,29 +8,34 @@ Frustration::Application.routes.draw do
   #-----------------------------------------------
   # web
   #-----------------------------------------------
-  match 'login'  => 'sessions#new', :as => :login
-  match 'logout' => 'sessions#destroy', :as => :logout
+  # login/out
+  match 'login'                    => 'sessions#new',     :as => :login
+  match 'logout'                   => 'sessions#destroy', :as => :logout
   match '/auth/:provider/callback' => 'sessions#create'
-  resources :sessions, :only => %w[new create destroy]
+  resources :sessions, :only       => %w[new create destroy]
 
-  match 'users/new' => 'users#new', :via => :get
+  # sign up
+  match 'users/new' => 'users#new',    :via => :get
   match 'users'     => 'users#create', :via => :post
 
+  # home
   match 'home' => 'home#index', :via => :get
-  #match 'search' => 'home#search', via: [:get, :post]
 
+  # setting
   scope '/settings', as: :settings do
-    match '/icon'  => 'settings#icon', via: :get
-    match '/profile'  => 'settings#profile', via: :get
-    match '/categories' => 'settings#categories', via: :get
-    match '/tags' => 'settings#tags', via: :get
+    match '/icon'       => 'settings#icon',    via: :get
+    match '/profile'    => 'settings#profile', via: :get
+    resources :categories
+    resources :tags
+    resources :comments
 
-    match '/twitter' => 'settings#twitter', via: :get
-    match '/facebook' => 'settings#facebook', via: :get
-    match '/gadget' => 'settings#gadget', via: :get
+    match '/twitter'    => 'settings#twitter',  via: :get
+    match '/facebook'   => 'settings#facebook', via: :get
+    match '/gadget'     => 'settings#gadget',   via: :get
   end
 
-  match 'fumans/search' => 'fumans#search', via: [:get, :post]
+  match 'fumans/'                 => 'fumans#index',      via: [:get, :post]
+  match 'fumans/search'           => 'fumans#search',     via: [:get, :post]
   match 'fumans/categories/:type' => 'fumans#categories', via: :get
   resources :fumans, :only => [:index] do
     collection do
@@ -38,7 +43,6 @@ Frustration::Application.routes.draw do
       post 'create_with_item'
     end
   end
-
 
   #-----------------------------------------------
   # api
