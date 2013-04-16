dfd89c93a994717e701ab25aa11ab5c24cf74983
nakajijapan <nakajima@paperboy.co.jp>
2013-04-16 21:58:02 +0900

    add profile

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,7 +2,28 @@ class SettingsController < ApplicationController
   before_filter :current_user
 
   def profile
-    #@user = User.find()
+    @user = @current_user
+  end
+
+  def profile_update
+    logger.warn params
+
+    @user = @current_user
+    @user.mode = :social
+    saved = @user.update_attributes(params[:user])
+
+    # response
+    respond_to do |format|
+      if saved
+        flash[:notice] = 'created!!'
+        format.html { redirect_to action: 'profile' }
+        format.json { render json: '', status: :created, location: @user }
+      else
+        logger.warn @user.errors.inspect
+        format.html { render action: 'profile' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def category
