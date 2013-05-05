5841fe0c231bec026dfde758ec2f4af68a03eac5
nakajijapan <nakajima@paperboy.co.jp>
2013-05-06 02:23:51 +0900

    bugfix modified target user

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe Friendship do
+
+  context 'when user following others' do
+    before do
+      @u        = create(:user_nakaji)
+      @u_target = create(:user_hamaji)
+      @f = Friendship.new({user_id: @u.id, following_id: @u_target.id})
+    end
+
+    it 'can save' do
+      @f.save
+    end
+
+    it 'can unfollow' do
+      @f.save
+      expect(@f.following_user.username).to eq @u_target.username
+    end
+
+  end
 end
