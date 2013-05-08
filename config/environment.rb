3e32369defbf6bff981514adabe9318050eec54a
nakajijapan <nakajima@paperboy.co.jp>
2013-05-09 01:57:17 +0900

    because it can not send gmail

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-if ENV['RAILS_ENV'] == 'production'
-  require File.expand_path('../../../shared/secret', __FILE__)
-elsif ENV['RAILS_ENV'] == 'test'
-  require File.expand_path('../secret_skel.rb', __FILE__)
-else
-  require File.expand_path('../secret', __FILE__)
-end
-
 # Initialize the rails application
 Frustration::Application.initialize!
 
