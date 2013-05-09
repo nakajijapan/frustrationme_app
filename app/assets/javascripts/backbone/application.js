b5fc5ef74ac706faaa34a5867700ac8b700c33a2
nakajijapan <nakajima@paperboy.co.jp>
2013-05-09 22:24:56 +0900

    delete string - .js

--- app/assets/javascripts/backbone/application.js
+++ app/assets/javascripts/backbone/application.js
@@ -1,7 +1,7 @@
 //= require backbone/underscore.js
 //= require backbone/backbone.js
-//= require ejs.js
-//= require ./common.js
+//= require ejs
+//= require ./common
 //= require_tree ./models/
 //= require_tree ./routers/
 //= require_tree ./views/
