24d7e71b7df2a64a93a824f8109fe341be67956e
nakajijapan <nakajima@paperboy.co.jp>
2013-05-09 22:05:04 +0900

    use localfile(.env)

--- config/boot.rb
+++ config/boot.rb
@@ -6,7 +6,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 if ENV['RAILS_ENV'] == 'production'
-  require File.expand_path('../../../shared/secret', __FILE__)
+  #require File.expand_path('../../../shared/secret', __FILE__)
 elsif ENV['RAILS_ENV'] == 'test'
   require File.expand_path('../secret_skel.rb', __FILE__)
 else
