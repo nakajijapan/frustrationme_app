610d2d7902e28d5cebb739202e54a99a66fa8aca
nakajijapan <nakajima@paperboy.co.jp>
2013-05-13 01:20:36 +0900

    no need 'before block'

--- spec/requests/api/fumans_spec.rb
+++ spec/requests/api/fumans_spec.rb
@@ -2,14 +2,9 @@
 require 'spec_helper'
 
 describe 'Api::Fumans' do
-  before do
-    @u = create(:user_nakaji)
-    @my_key = 'd41d8cd98f00b204e9800998ecf8427e'
-    @auth_str = "?my_user_id=#{@u.id}&my_key=#{@my_key}"
-  end
 
   describe '#statuses' do
-    it 'return 200' do
+    it 'returns http success' do
       get "/api/fumans/statuses.json"
       expect(response.status).to eq 200
     end
