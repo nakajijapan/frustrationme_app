6d4b8ff1de3b16e5f5d0b3c64381364cd553f108
nakajijapan <nakajima@paperboy.co.jp>
2013-06-01 19:02:44 +0900

    deleted no need lines

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,26 +6,19 @@ class SettingsController < ApplicationController
   end
 
   def profile_update
-    logger.warn params
-
     @user = @current_user
     @user.mode = :social
     saved = @user.update_attributes(params[:user])
 
-    # response
     respond_to do |format|
       if saved
         flash[:notice] = 'created!!'
         format.html { redirect_to action: 'profile' }
         format.json { render json: '', status: :created, location: @user }
       else
-        logger.warn @user.errors.inspect
         format.html { render action: 'profile' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
-  end
-
-  def category
   end
 end
