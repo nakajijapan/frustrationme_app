80a234962113a4f0598bd86fda2b60c04634f5c3
nakajijapan <nakajima@paperboy.co.jp>
2013-11-24 09:18:53 +0900

    move Mailer Settings to initializer

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Frustration::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'gmail.com',
-  :user_name            => ENV['MAIL_USERNAME'],
-  :password             => ENV['MAIL_PASSWORD'],
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-}
