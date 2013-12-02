9631fbbba281c4858f229226b48aee7f9d801dd7
nakajijapan <nakajima@paperboy.co.jp>
2013-12-02 10:59:01 +0900

    user must logged in

--- spec/requests/api/items_spec.rb
+++ spec/requests/api/items_spec.rb
@@ -7,6 +7,7 @@ describe 'Api::Items' do
     @u = create(:user_nakaji)
     @i = create(:item_amazon)
     @f = create(:fuman, {user_id: @u.id, item_id: @i.id})
+    cookies[:auth_token] = @u.auth_token
   end
 
   describe '/api/items/1' do
