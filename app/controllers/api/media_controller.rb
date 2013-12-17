f3fc35e9ecc7b885aca5fdc75b8f9489a7758092
nakajijapan <nakajima@paperboy.co.jp>
2013-12-17 11:14:42 +0900

    Changed method name

--- app/controllers/api/media_controller.rb
+++ app/controllers/api/media_controller.rb
@@ -2,7 +2,7 @@ class Api::MediaController < Api::ApplicationController
   skip_filter :require_user
 
   def public_timeline
-    @items = Item.index_list(params)
+    @items = Item.public_timeline(params)
   end
 
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,6 @@ class TopController < ApplicationController
   before_filter :check_user
 
   def index
-    @items = Item.index_list(params)
+    @items = Item.public_timeline(params)
   end
 end
