f2cd148cfc77313c844606586934c3c9b182fd1a
nakajijapan <nakajima@paperboy.co.jp>
2013-12-28 22:45:48 +0900

    add skelton file for newrelic

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :log_level, :debug
 set :pty, true
 
 # set :linked_files, %w{config/database.yml}
-set :linked_files, %w{config/secret.rb}
+set :linked_files, %w{config/secret.rb config/newrelic.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
