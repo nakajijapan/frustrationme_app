e0a64b758ed0fe7f904ed60bf7f24c0baeb820f3
nakajijapan <nakajima@paperboy.co.jp>
2013-12-28 20:34:52 +0900

    move setting

--- config/boot.rb
+++ config/boot.rb
@@ -5,10 +5,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-if ENV['RAILS_ENV'] == 'production'
-  #require File.expand_path('../env.rb', __FILE__)
-  require File.expand_path('../env.rb', __FILE__) if File.exists?(File.expand_path('../env.rb', __FILE__))
-elsif ENV['RAILS_ENV'] == 'test'
+if ENV['RAILS_ENV'] == 'test'
   require File.expand_path('../secret_skel.rb', __FILE__)
 else
   require File.expand_path('../secret', __FILE__)
--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-if ENV['RAILS_ENV'] == 'test'
-  require File.expand_path('../secret_skel.rb', __FILE__)
-else
-  require File.expand_path('../secret', __FILE__)
-end
-
 # Initialize the rails application
 Frustration::Application.initialize!
