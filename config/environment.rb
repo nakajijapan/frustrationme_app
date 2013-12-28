7df79735db6f1bf80ae7df259c270757ac896372
nakajijapan <nakajima@paperboy.co.jp>
2013-12-28 20:06:00 +0900

    changed env path

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-if ENV['RAILS_ENV'] == 'production'
-  require File.expand_path('../../../shared/secret', __FILE__)
-elsif ENV['RAILS_ENV'] == 'test'
+if ENV['RAILS_ENV'] == 'test'
   require File.expand_path('../secret_skel.rb', __FILE__)
 else
   require File.expand_path('../secret', __FILE__)
