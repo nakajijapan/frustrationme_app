cdc2dafd5af5d577beed1e28e29885f0a93dd4ff
nakajijapan <nakajima@paperboy.co.jp>
2013-12-28 19:52:23 +0900

    modified for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,21 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'frustration'
+set :repo_url, 'git@github.com:nakajijapan/frustrationme_app.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :deploy_to, '/var/www/app/'
+set :scm, :git
 
 # set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
+set :log_level, :debug
+set :pty, true
 
 # set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/secret.rb}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
