329194c225283f9f1d6eed63fc44eaa5cdb006d3
nakajijapan <nakajima@paperboy.co.jp>
2013-12-29 20:11:27 +0900

    invoke 'unicorn:restart'

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,20 +21,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-      invoke 'unicorn:restart'
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+    invoke 'unicorn:restart'
   end
 
   after :finishing, 'deploy:cleanup'
